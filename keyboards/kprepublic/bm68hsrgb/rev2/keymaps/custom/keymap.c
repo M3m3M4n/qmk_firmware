877b1438377ab50f08514048201f0a52442d90fb
hoang <nh231296@gmail.com>
2024-05-11 04:03:20 +0700

    changed

--- keyboards/kprepublic/bm68hsrgb/rev2/keymaps/custom/keymap.c
+++ keyboards/kprepublic/bm68hsrgb/rev2/keymaps/custom/keymap.c
@@ -33,6 +33,8 @@ enum _layers {
 // https://github.com/qmk/qmk_firmware/blob/master/docs/mod_tap.md#changing-both-tap-and-hold
 // If needed more change KC_
 #define MKC_LYR_ULT  LT(0, KC_0)         // Toggle between BASE and MAC layer when tap, UTIL when hold
+// Can just use LT(LAYER_UTIL, KC_CAPS) but has some conflict handing UTIL layer with MKC_LYR_ULT
+#define MKC_CAP_ULT  LT(0, KC_1)         // Toggle CAPSLOCK when tap, UTIL when hold
 
 enum _keycodes {
   // ==================================================
@@ -64,7 +66,29 @@ enum _keycodes {
 };
 
 // Manage own base layer, can be BASE or MAC
-static uint8_t base_layer = LAYER_BASE; 
+static uint8_t layer_base         = LAYER_BASE;
+static uint8_t layer_current      = LAYER_BASE;
+static uint8_t layer_prev         = LAYER_BASE;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  layer_prev = layer_current;
+  layer_current = get_highest_layer(state);
+  switch (layer_current) {
+  case LAYER_BASE:
+    layer_base = LAYER_BASE;
+    break;
+  case LAYER_MAC:
+    layer_base = LAYER_MAC;
+    break;
+  case LAYER_UTIL:
+    break;
+  case LAYER_SYS:
+    break;
+  default: //  for any other layers, or the default layer
+    break;
+  }
+  return state;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -143,13 +167,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Key being tap
         if (record->event.pressed) {
           // key tap down event
-          if (get_highest_layer(layer_state) == LAYER_BASE) {
-            // default_layer_set(1UL << 1); // Mac  == layer 2
+          uint8_t layer_tmp = get_highest_layer(layer_state);
+          if (layer_tmp == LAYER_BASE) {
+            // default_layer_set(1UL << 1); // Mac  == layer 2, this does not properly set layer info
             layer_move(LAYER_MAC);
           }
-          else {
-            // default_layer_set(1UL << 0); // Base == layer 1
+          else if (layer_tmp == LAYER_MAC) {
+            // default_layer_set(1UL << 0); // Base == layer 1, this does not properly set layer info
             layer_move(LAYER_BASE);
+          }
+          else {
+            // Update layer_base manually so util layers can return to it later
+            layer_base = ((layer_base == LAYER_BASE) ? LAYER_MAC : LAYER_BASE);
           }
         }
       }
@@ -157,12 +186,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Key being held
         if (record->event.pressed) {
           // key held down event
-          base_layer = get_highest_layer(layer_state);
-          layer_move(LAYER_UTIL);
+          if (get_highest_layer(layer_state) != LAYER_UTIL)
+            layer_move(LAYER_UTIL);
         }
         else {
           // key held up event
-          layer_move(base_layer);
+          if (get_highest_layer(layer_state) == LAYER_UTIL)
+            layer_move(layer_base);
+        }
+      }
+      return false;
+    case MKC_CAP_ULT:
+      if (record->tap.count) {
+        // Key being tap
+        if (record->event.pressed) {
+          // key tap down event
+          tap_code(KC_CAPS);
+        }
+      }
+      else {
+        // Key being held
+        if (record->event.pressed) {
+          // key held down event
+          if (get_highest_layer(layer_state) != LAYER_UTIL)
+            layer_move(LAYER_UTIL);
+        }
+        else {
+          // key held up event
+          if (get_highest_layer(layer_state) == LAYER_UTIL)
+            layer_move(layer_base);
         }
       }
       return false;
@@ -201,27 +253,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_65_ansi (
-    KC_GRV,      KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,        KC_MINS, KC_EQL,  KC_BSPC, LT(LAYER_SYS, KC_ESC),
-    KC_TAB,      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,        KC_LBRC, KC_RBRC, KC_BSLS, TD(TD_DEL_INS),
-    KC_CAPS,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,     KC_QUOT,          KC_ENT,  TD(TD_PGDN_PGUP),
-    KC_LSFT,                 KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   TD(TD_END_HOME),
-    KC_LCTL,     KC_LGUI,    KC_LALT,                                        KC_SPC,                             KC_RALT,    MKC_LYR_ULT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,          KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,        KC_MINS, KC_EQL,  KC_BSPC, LT(LAYER_SYS, KC_ESC),
+    KC_TAB,          KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,        KC_LBRC, KC_RBRC, KC_BSLS, TD(TD_DEL_INS),
+    MKC_CAP_ULT,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,     KC_QUOT,          KC_ENT,  TD(TD_PGDN_PGUP),
+    KC_LSFT,                     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   TD(TD_END_HOME),
+    KC_LCTL,         KC_LGUI,    KC_LALT,                                        KC_SPC,                             KC_RALT,    MKC_LYR_ULT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [LAYER_MAC] = LAYOUT_65_ansi (
-    KC_GRV,      KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,        KC_MINS, KC_EQL,  KC_BSPC, LT(LAYER_SYS, KC_ESC),
-    KC_TAB,      KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,        KC_LBRC, KC_RBRC, KC_BSLS, TD(TD_DEL_INS),
-    KC_CAPS,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,     KC_QUOT,          KC_ENT,  TD(TD_PGDN_PGUP),
-    KC_LSFT,                 KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   TD(TD_END_HOME),
-    KC_LCTL,     KC_LOPT,    KC_LCMD,                                        KC_SPC,                             KC_RCMD,    MKC_LYR_ULT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,          KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,        KC_MINS, KC_EQL,  KC_BSPC, LT(LAYER_SYS, KC_ESC),
+    KC_TAB,          KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,        KC_LBRC, KC_RBRC, KC_BSLS, TD(TD_DEL_INS),
+    MKC_CAP_ULT,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,     KC_QUOT,          KC_ENT,  TD(TD_PGDN_PGUP),
+    KC_LSFT,                     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,      KC_SLSH, KC_RSFT, KC_UP,   TD(TD_END_HOME),
+    KC_LCTL,         KC_LOPT,    KC_LCMD,                                        KC_SPC,                             KC_RCMD,    MKC_LYR_ULT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [LAYER_UTIL] = LAYOUT_65_ansi (
-    QK_BOOT,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,  KC_F12,  KC_F13,  KC_PSCR,
+    KC_ESC,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,  KC_F12,  KC_F13,  KC_PSCR,
 #if defined(RGBLIGHT_ENABLE)
     XXXXXXX,     RGB_TOG,    RGB_MOD,    RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_VAI,    RGB_VAD,    RGB_SPI,    RGB_SPD,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   #if defined(RGB_MATRIX_ENABLE)
-    XXXXXXX,     MKC_RM_TOG, MKC_RM_MOD, MKC_RM_HUI, MKC_RM_HUD, MKC_RM_SAI, MKC_RM_SAD, MKC_RM_VAI, MKC_RM_VAD, MKC_RM_SPI, MKC_RM_SPD,  XXXXXXX,          XXXXXXX, XXXXXXX,
+    _______,     MKC_RM_TOG, MKC_RM_MOD, MKC_RM_HUI, MKC_RM_HUD, MKC_RM_SAI, MKC_RM_SAD, MKC_RM_VAI, MKC_RM_VAD, MKC_RM_SPI, MKC_RM_SPD,  XXXXXXX,          XXXXXXX, XXXXXXX,
   #else
-    XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX,
+    _______,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX,
   #endif
 #else
   #if defined(RGB_MATRIX_ENABLE)
@@ -229,14 +281,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   #else
     XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   #endif
-    XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX,
+    _______,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX,
 #endif
-    _______,                 KC_VOLU,    KC_VOLD,    KC_MUTE,    XXXXXXX,    XXXXXXX,    NK_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, _______, XXXXXXX, XXXXXXX,
+    _______,                 KC_VOLD,    KC_VOLU,    KC_MUTE,    XXXXXXX,    XXXXXXX,    NK_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, _______, XXXXXXX, XXXXXXX,
     _______,     _______,    _______,                                        XXXXXXX,                            _______,    _______,     _______, XXXXXXX, XXXXXXX, XXXXXXX
   ),
   [LAYER_SYS] = LAYOUT_65_ansi (
     MKC_EREBOOT, _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    XXXXXXX,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     XXXXXXX, XXXXXXX, XXXXXXX, MKC_LISYSRQ,
+    QK_BOOT,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     XXXXXXX, XXXXXXX, XXXXXXX, MKC_LISYSRQ,
     XXXXXXX,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX,
     XXXXXXX,                 _______,    _______,    _______,    _______,    _______,    _______,    _______,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX,     XXXXXXX,    XXXXXXX,                                        XXXXXXX,                            XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -265,13 +317,12 @@ bool rgb_matrix_indicators_user() {
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-  uint8_t layer = get_highest_layer(layer_state);
-  bool layer_changed = (layer != led_last_layer);
+  bool layer_changed = (layer_current != led_last_layer);
   bool layer_cleanup = (led_cleanup_count > 0);
   if (layer_cleanup)
       led_cleanup_count--;
   if (layer_changed) {
-      led_last_layer    = layer;
+      led_last_layer    = layer_current;
       led_cleanup_count = 5; // 5 times seems enough
   }
   // Base color for special layers
@@ -286,8 +337,8 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         // If this layer has a base color for valid keycode, set those
         // Skip layer base and mac because those use led effect
         // All keys with invalid keycode are turned off 
-        if (keymap_key_to_keycode(layer, (keypos_t){col,row}) >= KC_TRNS) { // keycode valid
-          switch (layer) {
+        if (keymap_key_to_keycode(layer_current, (keypos_t){col,row}) >= KC_TRNS) { // keycode valid
+          switch (layer_current) {
             // We need to clear led because of these 2
             // If led effect not modify keys on the 2 layers then leftover will happend
             case LAYER_MAC:
@@ -311,7 +362,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   if (rgb_matrix_get_flags() == LED_FLAG_NONE)
     rgb_matrix_set_color(30, RGB_BLACK);
   // Special keys for each layer
-  switch(layer) {
+  switch(layer_current) {
     case LAYER_MAC:
       rgb_matrix_set_color(63, RGB_WHITE); // FN
       // Fall through
@@ -321,10 +372,10 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         rgb_matrix_set_color(30, RGB_RED);
       break;
     case LAYER_UTIL:
-      rgb_matrix_set_color(0 , RGB_RED);   // QK_BOOT
       break;
     case LAYER_SYS:
       rgb_matrix_set_color(0 , RGB_RED);   // MKC_EREBOOT
+      rgb_matrix_set_color(15, RGB_ORANGE);// QK_BOOT
       rgb_matrix_set_color(29, RGB_WHITE); // MKC_LISYSRQ
       break;
   }
