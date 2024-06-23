e8c9623efed0050e796928fc8fd69e8e1c455125
Hoang Nguyen <hoang@P16.lan>
2024-06-23 13:59:46 +0700

    update
    Committer: Hoang Nguyen <nh231296@gmail.com

--- keyboards/keychron/v1/ansi_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/custom/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2021 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 #include "config.h"
 
@@ -34,23 +18,23 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 enum _layers {
-    MAC_BASE = 0,
-    MAC_FN,
-    WIN_BASE,
-    WIN_FN
+    LAYER_BASE_MAC = 0,
+    LAYER_UTIL_MAC,
+    LAYER_BASE_WIN,
+    LAYER_UTIL_WIN
 };
 
 #ifdef DIP_SWITCH_ENABLE
 bool dip_switch_update_user(uint8_t index, bool active) {
     // This default implemetation does not properly move the layer, causing rgb matrix error
     // if (index == 0) {
-    //     default_layer_set(1UL << (active ? WIN_BASE : MAC_BASE));
+    //     default_layer_set(1UL << (active ? LAYER_BASE_WIN : LAYER_BASE_MAC));
     // }
     if (index == 0) {
         if (active)
-            layer_move(WIN_BASE);
+            layer_move(LAYER_BASE_WIN);
         else
-            layer_move(MAC_BASE);
+            layer_move(LAYER_BASE_MAC);
     }
     return false;
 }
@@ -62,6 +46,30 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 enum _keycodes {
     MKC_EREBOOT = SAFE_RANGE  // Emergency reboot
 };
+
+// Manage own base layer, can be BASE or MAC
+static uint8_t layer_base         = 0xff;
+static uint8_t layer_current      = 0xff;
+static uint8_t layer_prev         = 0xff;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    layer_prev = layer_current;
+    layer_current = get_highest_layer(state);
+    switch (layer_current) {
+        case LAYER_BASE_MAC:
+            layer_base = LAYER_BASE_MAC;
+            break;
+        case LAYER_BASE_WIN:
+            layer_base = LAYER_BASE_WIN;
+            break;
+        case LAYER_UTIL_MAC:
+        case LAYER_UTIL_WIN:
+            break;
+        default: //  for any other layers, or the default layer
+            break;
+    }
+    return state;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -89,46 +97,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_ansi_82(
-        KC_ESC,      KC_BRID,  KC_BRIU, KC_NO,    KC_NO,    RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,    KC_VOLU, KC_PSCR,          KC_MUTE,
-        KC_GRV,      KC_1,     KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_BSPC,          TD(TD_DEL_INS),
-        KC_TAB,      KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS,          TD(TD_PGDN_PGUP),
-        KC_CAPS,     KC_A,     KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           TD(TD_END_HOME),
-        KC_LSFT,               KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,
-        KC_LCTL,     KC_LOPT,  KC_LCMD,                              KC_SPC,                             KC_RCMD, MO(MAC_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    [LAYER_BASE_MAC] = LAYOUT_ansi_82(
+        KC_ESC,      KC_BRID,  KC_BRIU, KC_NO,    KC_NO,    RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,            KC_VOLU, KC_PSCR,          KC_MUTE,
+        KC_GRV,      KC_1,     KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,            KC_EQL,  KC_BSPC,          TD(TD_DEL_INS),
+        KC_TAB,      KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,            KC_RBRC, KC_BSLS,          TD(TD_PGDN_PGUP),
+        KC_CAPS,     KC_A,     KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                     KC_ENT,           TD(TD_END_HOME),
+        KC_LSFT,               KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                     KC_RSFT, KC_UP,
+        KC_LCTL,     KC_LOPT,  KC_LCMD,                              KC_SPC,                             KC_RCMD, MO(LAYER_UTIL_MAC), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
         ),
-    [MAC_FN] = LAYOUT_ansi_82(
-        MKC_EREBOOT, KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,  XXXXXXX,          _______,
-        QK_BOOT,     XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,          XXXXXXX,
-        RGB_TOG,     RGB_MOD,  RGB_VAI, RGB_HUI,  RGB_SAI,  RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,          XXXXXXX,
-        XXXXXXX,     RGB_RMOD, RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,          XXXXXXX,
-        XXXXXXX,               XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX,
-        XXXXXXX,     XXXXXXX,  XXXXXXX,                              XXXXXXX,                            XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    [LAYER_UTIL_MAC] = LAYOUT_ansi_82(
+        MKC_EREBOOT, KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,             KC_F12,  XXXXXXX,          _______,
+        QK_BOOT,     XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,          XXXXXXX,
+        RGB_TOG,     RGB_MOD,  RGB_VAI, RGB_HUI,  RGB_SAI,  RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX,     RGB_RMOD, RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX,          XXXXXXX,
+        XXXXXXX,               XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,
+        XXXXXXX,     XXXXXXX,  XXXXXXX,                              XXXXXXX,                            XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
         ),
-    [WIN_BASE] = LAYOUT_ansi_82(
-        KC_ESC,      KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,      KC_1,     KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_BSPC,          TD(TD_DEL_INS),
-        KC_TAB,      KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS,          TD(TD_PGDN_PGUP),
-        KC_CAPS,     KC_A,     KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           TD(TD_END_HOME),
-        KC_LSFT,               KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,
-        KC_LCTL,     KC_LCMD,  KC_LALT,                              KC_SPC,                             KC_RALT, MO(WIN_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    [LAYER_BASE_WIN] = LAYOUT_ansi_82(
+        KC_ESC,      KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,             KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_GRV,      KC_1,     KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,            KC_EQL,  KC_BSPC,          TD(TD_DEL_INS),
+        KC_TAB,      KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,            KC_RBRC, KC_BSLS,          TD(TD_PGDN_PGUP),
+        KC_CAPS,     KC_A,     KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                     KC_ENT,           TD(TD_END_HOME),
+        KC_LSFT,               KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                     KC_RSFT, KC_UP,
+        KC_LCTL,     KC_LCMD,  KC_LALT,                              KC_SPC,                             KC_RALT, MO(LAYER_UTIL_WIN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
         ),
-    [WIN_FN] = LAYOUT_ansi_82(
-        MKC_EREBOOT, KC_BRID,  KC_BRIU, MKC_TASK, MKC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,    KC_VOLU, XXXXXXX,          _______,
-        QK_BOOT,     XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,          XXXXXXX,
-        RGB_TOG,     RGB_MOD,  RGB_VAI, RGB_HUI,  RGB_SAI,  RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,          XXXXXXX,
-        XXXXXXX,     RGB_RMOD, RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,          XXXXXXX,
-        XXXXXXX,               XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX,
-        XXXXXXX,     XXXXXXX,  XXXXXXX,                              XXXXXXX,                            XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    [LAYER_UTIL_WIN] = LAYOUT_ansi_82(
+        MKC_EREBOOT, KC_BRID,  KC_BRIU, MKC_TASK, MKC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,            KC_VOLU, XXXXXXX,          _______,
+        QK_BOOT,     XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,          XXXXXXX,
+        RGB_TOG,     RGB_MOD,  RGB_VAI, RGB_HUI,  RGB_SAI,  RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX,     RGB_RMOD, RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX,          XXXXXXX,
+        XXXXXXX,               XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,
+        XXXXXXX,     XXXXXXX,  XXXXXXX,                              XXXXXXX,                            XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
         )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [MAC_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
+    [LAYER_BASE_MAC] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [LAYER_UTIL_MAC] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [LAYER_BASE_WIN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [LAYER_UTIL_WIN] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
 };
 #endif // ENCODER_MAP_ENABLE
 
@@ -151,7 +159,7 @@ bool rgb_matrix_indicators_user() {
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t layer = get_highest_layer(layer_state);
-    bool layer_changed = (layer != led_last_layer);
+    bool layer_changed = (layer_current != led_last_layer);
     bool layer_cleanup = (led_cleanup_count > 0);
     if (layer_cleanup)
         led_cleanup_count--;
@@ -166,20 +174,21 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             uint8_t index = g_led_config.matrix_co[row][col];
             if ((index >= led_min) && (index < led_max) && (index != NO_LED)) { // Led valid
                 // Clear all the key if needed
+                // Faster than comparing individual key as div takes time
                 if (layer_changed || layer_cleanup)
                     rgb_matrix_set_color(index, RGB_BLACK);
                 // If this layer has a base color for valid keycode, set those
                 // Skip layer base and mac because those use led effect
                 // All keys with invalid keycode are turned off 
                 if (keymap_key_to_keycode(layer, (keypos_t){col,row}) >= KC_TRNS) { // keycode valid
-                    switch (layer) {
+                    switch (layer_current) {
                         // We need to clear led because of these 2
                         // If led effect not modify keys on the 2 layers then leftover will happend
-                        case MAC_BASE:
-                        case WIN_BASE:
+                        case LAYER_BASE_MAC:
+                        case LAYER_BASE_WIN:
                             break;
-                        case MAC_FN:
-                        case WIN_FN:
+                        case LAYER_UTIL_MAC:
+                        case LAYER_UTIL_WIN:
                             rgb_matrix_set_color(index, RGB_CYAN);
                             break;
                     }
@@ -195,16 +204,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         rgb_matrix_set_color(45, RGB_BLACK);
     // Special keys for each layer
     switch(layer) {
-        case MAC_BASE:
-            rgb_matrix_set_color(75, RGB_WHITE); // SPC
+        case LAYER_BASE_MAC:
             // Fall through
-        case WIN_BASE:
+        case LAYER_BASE_WIN:
+            rgb_matrix_set_color(73, RGB_RED); // WIN
             // Caps indicator
             if (host_keyboard_led_state().caps_lock)
                 rgb_matrix_set_color(45, RGB_RED);
             break;
-        case MAC_FN:
-        case WIN_FN:
+        case LAYER_UTIL_MAC:
+        case LAYER_UTIL_WIN:
             rgb_matrix_set_color(0 , RGB_RED);     // EMERG reboot - esc
             rgb_matrix_set_color(15 , RGB_ORANGE); // QK_BOOT - grave
             break;
